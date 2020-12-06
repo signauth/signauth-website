a28557895422ef4e5d1e448f9d666edc247de4c6
Francesco Sullo <sullof@gmail.com>
2020-12-05 22:52:34 -0800

    Minor fix

--- client/components/Home.js
+++ client/components/Home.js
@@ -7,6 +7,7 @@ import ReactMarkdown from 'react-markdown'
 import Base from './Base'
 
 const markdown = ` ## Introduction
+
 Maybe you don't know it, but any major web service (Twitter, Pinterest, etc.) sends your password to their server in a plain text. Only after, they derivate the password and save it in the database. What happens is a smart employee puts a backdoor at the beginning of the flow and steal your password?
 
 If you ask Twitter, they will tell you that their processes are totally secure. I.e., an employee would never be able to do that without being spotted immediately.
@@ -15,10 +16,10 @@ Let me say that I worked in many large companies, and security is not what they 
 
 The point is that we cannot trust them, and they should not trust themselves. The solution is to solve the issue in the browser.
 
+
 ## The SignAuth approach
 
-What if you could use your username and password to generate an ED25519 pair of keys and sign a challenge with them?
-If you could do this, the server could verify the signature and save just your public key in its database.
+What if you could use your username and password to generate an ED25519 pair of keys and sign a challenge with them? If you could do this, the server could verify the signature and save just your public key in its database.
 
 This is how precisely SignAuth works. Look at the following flow chart:
 
@@ -30,10 +31,9 @@ To check how it works, if you are an engineer, you can inspect the network and s
 
 ## Contributions
 
-My name is Francesco Sullo, and I am fascinated by security and, mostly, by how little people care about it.
- I had the idea for SignAuth after a discussion with a friend who works for Pinterest. It put me about one day to write the main library and another day to build a boilerplate to help people implementing SignAuth—I used the boilerplate as a template for this website.
+My name is Francesco Sullo, and I am fascinated by security and, mostly, by how little people care about it. I had the idea for SignAuth after a discussion with a friend who works for Pinterest. It put me about one day to write the main library and another day to build a boilerplate to help people implementing SignAuth — I used the boilerplate as a template for this website.
 
- The way SignAuth works is not a new idea; it is actually similar to how many other protocols. Still, the concept is quite powerful, and I hope that other developers would like to help me improving the protocol.
+The way SignAuth works is not a new idea; it is actually similar to many other protocols. Still, the concept is quite powerful, and I hope that other developers would like to help me improving the protocol.
 
 For more info, look at the GitHub repos [https://github.com/signauth](https://github.com/signauth)
 
